54a2d922841219fc23ba778ec35f5627a3456b05
Duc T. Lam <ducl_13@icloud.com>
2021-03-26 16:14:05 -0700

    Add mobile image

--- app/models/spree/slide.rb
+++ app/models/spree/slide.rb
@@ -6,10 +6,12 @@ class Spree::Slide < ActiveRecord::Base
   belongs_to :product, touch: true, optional: true
 
   has_one_attached :image
+  has_one_attached :mobile_image
 
   validates :link_url, presence: true, url: true, unless: -> { product }
-  validates :name, :image, presence: true, unless: -> { product }
+  validates :name, :image, :mobile_image, presence: true, unless: -> { product }
   validates :image, content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
+  validates :mobile_image, content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']
 
   scope :published, -> { where(published: true).order('position ASC') }
   scope :location, ->(location) { joins(:slide_locations).where('spree_slide_locations.name = ?', location) }
@@ -38,18 +40,34 @@ class Spree::Slide < ActiveRecord::Base
     !image.attached? && product.present? && product.images.any? ? product.images.first.attachment : image.attachment
   end
 
+  def slide_mobile_image
+    !mobile_image.attached? && product.present? && product.images.any? ? product.images.first.attachment : mobile_image.attachment
+  end
+
   # Helper for resizing
   def preview
     image_form(:preview)
+  end
+
+  def mobile_preview
+    mobile_image_form(:preview)
   end
 
   def thumbnail
     image_form(:thumbnail)
   end
 
+  def mobile_thumbnail
+    mobile_image_form(:thumbnail)
+  end
+
   private
 
   def image_form(form)
     slide_image.variant(resize: STYLES[form])
+  end
+
+  def mobile_image_form(form)
+    slide_mobile_image.variant(resize: STYLES[form])
   end
 end
